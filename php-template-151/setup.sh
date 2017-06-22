adf354e42ba3a757f9070f3ccf7f7eeb3caa8c8e
Ofaso <kevinwuestner5@gmail.com>
2017-06-22 18:30:55 +0200

    Bullshit git

--- php-template-151/setup.sh
+++
@@ -1,6 +0,0 @@
-# This scripts changes the volumes to be writable for the www-data user
-
-#82 is the user-id of www-data inside the container
-docker-compose run --rm -u root fpm sh -c "chown -R 82 .."
-docker-compose run --rm fpm composer install
-docker-compose down
